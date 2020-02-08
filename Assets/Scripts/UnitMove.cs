5582558706266eee64921812870e778ba77eeef3
Haedong-Jeon <zezernlwhr@hanmail.net>
2020-02-08 20:59:34 +0900

    공격모션 추가

--- Assets/Scripts/UnitMove.cs
+++ Assets/Scripts/UnitMove.cs
@@ -16,15 +16,18 @@ public class UnitMove : MonoBehaviour
     {
         moveTargetPos = new Vector3(0,0,0);
     }
-
     void Update()
     {  
         if(Input.GetMouseButton(0))
             moveTargetPos = GetMovePos();
 
+        if(Input.GetMouseButton(1))
+        {
+            Attack();
+        }
+
         CharMove(moveTargetPos);
     }
-
     Vector3 GetMovePos()
     {
         Vector3 destination = new Vector3(0,0,0);
@@ -33,18 +36,16 @@ public class UnitMove : MonoBehaviour
 
         if(Physics.Raycast(ray,out hit,Mathf.Infinity))       
             destination = hit.point;
-        
+    
         transform.LookAt(hit.point);
         return destination;
     }
-
     void CharMove(Vector3 moveTargetPos)
     {
         Vector3 smoothPosition = Vector3.SmoothDamp(transform.position, moveTargetPos, ref lastMovingVelocity, smoothTime);
         transform.position = smoothPosition;
         MovingAnimation(moveTargetPos);
     }
-
     void MovingAnimation(Vector3 moveTargetPos)
     {
         animator.SetBool("Moving",true);
@@ -52,13 +53,23 @@ public class UnitMove : MonoBehaviour
         animator.SetFloat("Velocity Z", moveTargetPos.z - transform.position.z);
     }
 
+    void Attack()
+    {
+        animator.SetTrigger("AttackTrigger");
+        animator.SetBool("Moving",true);
+        animator.SetInteger("Weapon",1);
+        animator.SetInteger("Action",1);
+    }
     void FootL()
     {
-        //구매한 에셋에 Read-only 타입으로 지정된 애니메이션 이벤트때문에 만든 빈 함수
+        //구매한 에셋에 있는 Read-only 애니메이션 이벤트 때문에 만든 빈 함수
     }
-
     void FootR()
     {
-        //구매한 에셋에 Read-only 타입으로 지정된 애니메이션 이벤트때문에 만든 빈 함수
+        //구매한 에셋에 있는 Read-only 애니메이션 이벤트 때문에 만든 빈 함수
+    }
+    void Hit()
+    {
+        //구매한 에셋에 있는 Read-only 애니메이션 이벤트 때문에 만든 빈 함수
     }
 }
