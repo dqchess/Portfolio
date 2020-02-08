b3a2c38d95889e49f1d6fb88c03933add04e3d82
Haedong-Jeon <zezernlwhr@hanmail.net>
2020-02-08 11:49:16 +0900

    클릭한 좌표까지 캐릭터 이동 구현

---
+++ Assets/Scripts/CameraMove.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour
+{
+    Vector3 screenMoveTargetPos;
+
+    void Awake()
+    {
+        screenMoveTargetPos = transform.position;
+    }
+    void Update () 
+    {
+        if(Input.GetMouseButton(0))
+            screenMoveTargetPos = GetClickedPos();
+        
+        transform.position = Vector3.Lerp(transform.position,screenMoveTargetPos, 0.025f);       
+    }
+    Vector3 GetClickedPos()
+    {
+        Vector3 clickedPos = new Vector3(Input.mousePosition.x, 10, Input.mousePosition.y);
+        Camera.main.ScreenToWorldPoint(clickedPos);
+        Debug.Log("move to position : " + clickedPos.ToString());
+
+        return clickedPos;
+    }
+}
---
+++ Assets/Scripts/UnitMove.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class UnitMove : MonoBehaviour
+{
+    Vector3 moveTargetPos;
+    void Start()
+    {
+        moveTargetPos = transform.position;
+    }
+    void Update()
+    {  
+        if(Input.GetMouseButton(0))
+            moveTargetPos = GetMovePos();
+
+        CharMove(moveTargetPos);
+    }
+    Vector3 GetMovePos()
+    {
+        Vector3 destination = new Vector3(0,0,0);
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if(Physics.Raycast(ray,out hit,Mathf.Infinity))
+        {
+            destination = hit.point;
+            Debug.Log("Raycast Hit...!..."+hit.point.ToString());
+        }
+        return destination;
+    }
+    void CharMove(Vector3 moveTargetPos)
+    {
+        transform.position = Vector3.Lerp(transform.position,moveTargetPos, 0.035f);
+    }
+}
