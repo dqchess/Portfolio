2b349f99761bf45f6ccb61686af978963a5ff12f
Haedong-Jeon <zezernlwhr@hanmail.net>
2020-02-08 19:11:15 +0900

    움직임 애니메이션 추가

--- Assets/Scripts/UnitMove.cs
+++ Assets/Scripts/UnitMove.cs
@@ -4,10 +4,19 @@ using UnityEngine;
 public class UnitMove : MonoBehaviour
 {
     Vector3 moveTargetPos;
+    Vector3 lastMovingVelocity;
+    Animator animator;
+    public float smoothTime;
+
+    void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
     void Start()
     {
         moveTargetPos = new Vector3(0,0,0);
     }
+
     void Update()
     {  
         if(Input.GetMouseButton(0))
@@ -15,20 +24,41 @@ public class UnitMove : MonoBehaviour
 
         CharMove(moveTargetPos);
     }
+
     Vector3 GetMovePos()
     {
         Vector3 destination = new Vector3(0,0,0);
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         RaycastHit hit;
-        if(Physics.Raycast(ray,out hit,Mathf.Infinity))
-        {
+
+        if(Physics.Raycast(ray,out hit,Mathf.Infinity))       
             destination = hit.point;
-        }
+        
         transform.LookAt(hit.point);
         return destination;
     }
+
     void CharMove(Vector3 moveTargetPos)
     {
-        transform.position = Vector3.Lerp(transform.position,moveTargetPos, 0.035f);
+        Vector3 smoothPosition = Vector3.SmoothDamp(transform.position, moveTargetPos, ref lastMovingVelocity, smoothTime);
+        transform.position = smoothPosition;
+        MovingAnimation(moveTargetPos);
+    }
+
+    void MovingAnimation(Vector3 moveTargetPos)
+    {
+        animator.SetBool("Moving",true);
+        animator.SetFloat("Velocity X", moveTargetPos.x - transform.position.x);
+        animator.SetFloat("Velocity Z", moveTargetPos.z - transform.position.z);
+    }
+
+    void FootL()
+    {
+        //구매한 에셋에 Read-only 타입으로 지정된 애니메이션 이벤트때문에 만든 빈 함수
+    }
+
+    void FootR()
+    {
+        //구매한 에셋에 Read-only 타입으로 지정된 애니메이션 이벤트때문에 만든 빈 함수
     }
 }
